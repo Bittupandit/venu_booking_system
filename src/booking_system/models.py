34758ce43cd1066e1572090107a0eee1144d3099
Surendra Mishra <surendra.mishra@arohatech.com>
2021-06-27 21:28:51 +0530

    changes

--- src/booking_system/models.py
+++ src/booking_system/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.base import Model
 from django.db.models.fields.files import ImageField
 
 # Create your models here.
@@ -6,8 +7,29 @@ from django.db.models.fields.files import ImageField
 
 # booking system model...
 
+CITY_TYPE = (
+    ('Delhi','Delhi'),
+    ('Greate Noida','Greate Noida'),
+    ('Lucknow','Lucknow'),
+    ('Kanpur','Kanpur'),
+    ('Muradabad','Muradabad'),
+    ('Barabanki','Barabanki'),
+    ('Varanasi','Varanasi'),
+    ('Azamgarh','Azamgarh'),
+    
+)
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=200,blank=True, null=True,verbose_name="Location Name")
+    city = models.CharField(max_length=30,choices=CITY_TYPE,blank=True, null=True,verbose_name="City Name")
+
+    def __str__(self):
+        return f'{self.name}'
+
 class BookingSystem(models.Model):
     name = models.CharField(max_length=250,blank=True, null=True,verbose_name='Venu Name',help_text="Enter the Venue Name: ")
+    location = models.ForeignKey(Location,on_delete=models.CASCADE,blank=True, null=True)
     venue_image = models.ImageField(upload_to = 'venu_image')
     address = models.CharField(max_length=300,blank=True, null=True,verbose_name="Address")
     landmark = models.CharField(max_length=100,blank=True, null=True,verbose_name="Landmark")
--- src/booking_system/views.py
+++ src/booking_system/views.py
@@ -2,5 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 
+from .models import *
+
 def home_page(request):
-    return render(request,'home_page.html')
+    qs = BookingSystem.objects.all()
+    context = {'venues':qs}
+    return render(request,'home_page.html',context)
+
+def detail_page(request,pk):
+    qs = BookingSystem.objects.get(pk=pk)
+    context = {'venue':qs}
+    return render(request,'detail_page.html',context)
